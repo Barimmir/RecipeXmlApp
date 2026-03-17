08a77cfd5f47e5c71fb8389e78dfd20ac7c77e4d
GrivnoKoder <grivnoisinsaideme@gmail.com>
2026-03-17 11:45:59 +0300

    RA-04-Создать первый фрагмент с категориями

--- app/src/main/java/com/example/recipexmlapp/MainActivity.kt
+++ app/src/main/java/com/example/recipexmlapp/MainActivity.kt
@@ -11,10 +11,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        
+        // Launch CategoriesListFragment
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.mainContainer, CategoriesListFragment())
+            .commit()
     }
 }
