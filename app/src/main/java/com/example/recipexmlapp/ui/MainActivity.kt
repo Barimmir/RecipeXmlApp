49bb9e549c7282094cfe62fc423447cc2824850d
barimmir <rrozial535@gmail.com>
2026-04-13 14:17:05 +0300

    RA-32-Реализовать запрос через URLConnection

--- app/src/main/java/com/example/recipexmlapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/recipexmlapp/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.recipexmlapp.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -8,28 +9,39 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.findNavController
 import com.example.recipexmlapp.R
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        
+
         val btnRecipes = findViewById<Button>(R.id.btn_recipes)
         val btnFavorites = findViewById<Button>(R.id.btn_favorites)
-        
+
         btnRecipes.setOnClickListener {
             findNavController(R.id.nav_host_fragment).navigate(R.id.categoriesListFragment)
         }
-        
+
         btnFavorites.setOnClickListener {
             findNavController(R.id.nav_host_fragment).navigate(R.id.favoritesFragment)
         }
+        val tread = Thread {
+            val url = URL("https://recipes.androidsprint.ru/api/category")
+            val connection = url.openConnection() as HttpURLConnection
+            connection.content
+            Log.i("!!!", "responseCode: ${connection.responseCode}")
+            Log.i("!!!", "responseMessage: ${connection.responseMessage}")
+            Log.i("!!!", "Body: ${connection.getInputStream().bufferedReader().readText()}")
+        }
+        tread.start()
     }
 }
