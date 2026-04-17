fd56a85d21afad035e8411868f129ebd6ca9cd89
barimmir <rrozial535@gmail.com>
2026-04-17 11:54:44 +0300

    RA-36-Подправил недочёт в api

--- app/src/main/java/com/example/recipexmlapp/data/ApiConstants.kt
+++ app/src/main/java/com/example/recipexmlapp/data/ApiConstants.kt
@@ -2,5 +2,5 @@ package com.example.recipexmlapp.data
 
 object ApiConstants {
     const val BASE_URL = "https://recipes.androidsprint.ru/api/"
-    const val IMAGE_BASE_URL = "https://recipes.androidsprint.ru/images/"
+    const val IMAGE_BASE_URL = "https://recipes.androidsprint.ru/api/images/"
 }
